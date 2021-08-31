c9d2ad1808eb242aabfc80d98540a6e37eae3bd5
Alex Segura <39310711+alexsegura1@users.noreply.github.com>
2021-08-31 17:12:56 -0600

    Add two more challenges

--- LinqChallengesFivePack/LinqChallenge.Test/LinqTests.cs
+++ LinqChallengesFivePack/LinqChallenge.Test/LinqTests.cs
@@ -24,5 +24,33 @@ namespace LinqChallenge.Test
 
             CollectionAssert.AreEqual(results, sut);
         }
+
+        [TestMethod]
+        public void PlayersWithBirthdayAndAgeSorted()
+        {
+            var challenges = new LinqChallenges();
+            List<Player> players = new List<Player>()
+            {
+                new Player(){ Name = "Jos Sagan", Birthday = new System.DateTime(1983, 4, 22)},
+                new Player(){ Name = "James Hansen", Birthday = new System.DateTime(1983, 10, 5)},
+                new Player(){ Name =  "Mariah Davis", Birthday = new System.DateTime(1985, 09, 08)},
+                new Player(){ Name = "Jeff Prosise", Birthday = new System.DateTime(1986, 04, 01)},
+                new Player(){ Name = "Ally Shaw", Birthday = new System.DateTime(1995, 12, 22)}
+            };
+
+            var sut = challenges.PlayerAgeSort();
+
+            CollectionAssert.AreEqual(players, sut);
+        }
+
+        [TestMethod]
+        public void CalculateAlbumTrackLengthSeconds()
+        {
+            var challenges = new LinqChallenges();
+            var sut = challenges.CalculateAlbumDuration();
+            Assert.AreEqual(sut, 100);
+        }
+
+
     }
 }
--- LinqChallengesFivePack/LinqChallengesFivePack/LinqChallenges.cs
+++ LinqChallengesFivePack/LinqChallengesFivePack/LinqChallenges.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqChallengesFivePack
 {
@@ -7,23 +8,35 @@ namespace LinqChallengesFivePack
     {
         public List<string> PlayerAssignJersey()
         {
-            var players = GetPlayerRoster();
+            var players =  "Smith, Robinson, Saenz, Cabanig, Jefferson";
+            int index = 0;
             //Assign jersey numbers here.
+
             return new List<string>();
+            
         }
 
-        private List<string> GetPlayerRoster()
+
+        public List<Player> PlayerAgeSort()
         {
-            return new List<string>()
-            {
-                "Smith",
-                "Robinson",
-                "Saenz",
-                "Cabanig",
-                "Jefferson"
-            };
+            var players = "Jeff Prosise, 04/01/1986; Jos Sagan, 04/22/1983; Mariah Davis, 09/08/1985; Ally Shaw, 12/22/1995; Hector Ramirez, 02/12/1991; James Hansen, 10/05/1983";
+            //Return list of players, sorted oldest to youngest
+            return new List<Player>();
         }
 
 
+        public int CalculateAlbumDurationSeconds()
+        {
+            string albumTrackLengths = "4:12,2:43,3:51,4:29,3:24,3:14,4:46,3:25,4:52,3:27";
+            //Calculate how long the album is, in seconds, given each track length
+            return 0;
+        }
+
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
     }
 }
