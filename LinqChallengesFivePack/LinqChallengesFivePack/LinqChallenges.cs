2f5d5e97d2e7247fc272b549929ceb0061ea41b8
Jordan Liberman <jliberman@degreed.com>
2021-09-13 14:15:19 -0700

    Jordan Liberman's LINQ challenge entry

--- LinqChallengesFivePack/LinqChallengesFivePack/LinqChallenges.cs
+++ LinqChallengesFivePack/LinqChallengesFivePack/LinqChallenges.cs
@@ -16,9 +16,14 @@ namespace LinqChallengesFivePack
         //Challenge: Given a string of player names give each player a jersey # starting at 1 and return back in a List i.e. "1. Smith", "2. Robinson" etc..
         public List<string> PlayerAssignJersey()
         {
-            var players =  "Smith, Robinson, Saenz, Cabanig, Jefferson";
+            var players = "Smith, Robinson, Saenz, Cabanig, Jefferson";
 
-            return new List<string>();
+            var playerList = players
+                .Split(',')                                                     //Split the input to get each name
+                .Select((x, index) => $"{index + 1}. {x.Trim()}")               //Add an index variable inside the lambda, return a formatted string
+                .ToList();                                                      //Return a list
+
+            return playerList;
             
         }
 
@@ -26,16 +31,27 @@ namespace LinqChallengesFivePack
         public List<Player> PlayerAgeSort()
         {
             var players = "Jeff Prosise, 04/01/1986; Jos Sagan, 04/22/1983; Mariah Davis, 09/08/1985; Ally Shaw, 12/22/1995; Hector Ramirez, 02/12/1991; James Hansen, 10/05/1983";
-           
-            return new List<Player>();
+
+            var playerList = players
+                .Split(';')                                                                                                 //Split the input to get each name
+                .Select(x => new Player() { Birthday = DateTime.Parse(x.Split(',')[1]), Name = x.Split(',')[0].Trim() })    //Parse each string into a Player object
+                .OrderBy(x => x.Birthday)                                                                                   //Order by birthday to get oldest player first
+                .ToList();                                                                                                  //Return a list
+
+            return playerList;
         }
 
         //Calculate how long the album is, in seconds, given track lengths
         public double CalculateAlbumDurationSeconds()
         {
             string albumTrackLengths = "4:12,2:43,3:51,4:29,3:24,3:14,4:46,3:25,4:52,3:27";
+
+            var totalSeconds = albumTrackLengths
+                .Split(',')                                                                         //Split the input to get each value
+                .Select(x => Double.Parse(x.Split(':')[1]) + (Double.Parse(x.Split(':')[0]) * 60))  //Parse each value by adding the seconds to minutes*60
+                .Sum();                                                                             //Sum up the seconds
             
-            return 0;
+            return totalSeconds;
         }
 
 
@@ -44,8 +60,16 @@ namespace LinqChallengesFivePack
         //Output should be: "0,0" "0,1" "0,2" "1,0" "1,1" "1,2" "2,0" "2,1" "2,2"
         public List<string> CalculateMatrixPoints3x3()
         {
-            
-            return new List<string>();
+            var min = 0;
+            var max = 2;
+
+            var list = Enumerable.Range(min, max + 1).ToList();     //Create a list of the min - max values, 0-2
+
+            var output = list
+                .SelectMany(x => list.Select(y => $"{x},{y}"))      //Cross join the list to itself, return a formatted string
+                .ToList();                                          //Return a list
+                
+            return output;
         }
 
         //Given the input, return a collection of integers with the ranges filled in
@@ -54,7 +78,21 @@ namespace LinqChallengesFivePack
         {  
             var input = "2,5,7-10,11,17-18";
 
-            return new List<int>();
+            var output = input
+                .Split(',')                                                                                     //Split the input to get each value
+                .Select(x =>                                                                                    //Parse each value as follows...
+                    {
+                        var num = x.Split('-');                                                                 //Split the string to see if it's a discrete value or a range
+
+                        if (num.Length == 1) { return new List<int>() { int.Parse(num[0]) }; }                  //For discreet values, return that number
+
+                        return Enumerable.Range(int.Parse(num[0]), int.Parse(num[1]) - int.Parse(num[0]) + 1);  //For ranges, use Enumerable.Range to expand it
+                    })
+                .SelectMany(x => x)                                                                             //Select Many flattens the List of Lists into a single list
+                .OrderBy(x => x)                                                                                //Ensure the numbers returned are in ascending order
+                .ToList();                                                                                      //Return a list
+
+            return output;
         }
 
     }
@@ -73,6 +111,16 @@ namespace LinqChallengesFivePack
             }
 
             return player.Name == Name && player.Birthday == Birthday;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name + Birthday.ToString()).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} / {Birthday}";
         }
     }
 }
