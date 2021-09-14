47100e373eddee12ed018a4a4eacfbbdb487d3a3
JLajarinDeg <jaime@degreed.com>
2021-09-14 11:07:17 -0500

    Update LinqChallenges.cs

--- LinqChallengesFivePack/LinqChallengesFivePack/LinqChallenges.cs
+++ LinqChallengesFivePack/LinqChallengesFivePack/LinqChallenges.cs
@@ -17,8 +17,9 @@ namespace LinqChallengesFivePack
         public List<string> PlayerAssignJersey()
         {
             var players =  "Smith, Robinson, Saenz, Cabanig, Jefferson";
+            var numberedPlayers = players.Split(',').Select((x, index) => $"{index+1}. {x.Trim()}").ToList();
 
-            return new List<string>();
+            return numberedPlayers;
             
         }
 
@@ -26,16 +27,16 @@ namespace LinqChallengesFivePack
         public List<Player> PlayerAgeSort()
         {
             var players = "Jeff Prosise, 04/01/1986; Jos Sagan, 04/22/1983; Mariah Davis, 09/08/1985; Ally Shaw, 12/22/1995; Hector Ramirez, 02/12/1991; James Hansen, 10/05/1983";
-
-            return new List<Player>();
+            var xPlayers = players.Split(';').Select(x => x.Split(',')).Select(x => new Player() { Name = x[0], Birthday = DateTime.Parse(x[1].Trim()) }).OrderBy(x => x.Birthday).ToList();
+            return xPlayers;
         }
 
         //Calculate how long the album is, in seconds, given track lengths
         public double CalculateAlbumDurationSeconds()
         {
             string albumTrackLengths = "4:12,2:43,3:51,4:29,3:24,3:14,4:46,3:25,4:52,3:27";
-            
-            return 0;
+            var x = albumTrackLengths.Split(',').Select(x => x.Split(':')).Select(x => int.Parse(x[0]) * 60 + int.Parse(x[1])).Sum();
+            return x;
         }
 
 
@@ -51,10 +52,16 @@ namespace LinqChallengesFivePack
         //Given the input, return a collection of integers with the ranges filled in
         //Solution should be: 2, 5, 7, 8, 9, 10, 11, 17, 18
         public List<int> GetRangesFromString()
-        {  
+        {
             var input = "2,5,7-10,11,17-18";
+            //var x = input.Split(',').Select(x => x.Contains('-')
+            //? Enumerable.Range(int.Parse(x.Substring(0, x.IndexOf('-'))), int.Parse(x.Substring(x.IndexOf('-') + 1)) - int.Parse(x.Substring(0, x.IndexOf('-')))).Select(x=>x.ToString()).Aggregate((concat, str) => $"{concat},{str}")
+            //: x
+            //).Aggregate((concat, str) => $"{concat},{str}"); 
 
-            return new List<int>();
+            var x = input.Split(',').Select(x => x.Split('-')).Select(x => Enumerable.Range(int.Parse(x[0]), x.Length == 1 ? 1 : int.Parse(x[1]) - int.Parse(x[0]) + 1)).SelectMany(x => x).ToList();      
+                  
+            return x;
         }
 
     }
